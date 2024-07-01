d90ea2d2e8ed0eb30882b7dd0dd802f4748e290a
Subhroneel Ganguly <subhro1976@gmail.com>
2024-07-02 00:58:51 +0530

    Update ScalaJdbcConnectMySQL.scala

--- ScalaJdbcConnectMySQL.scala
+++ ScalaJdbcConnectMySQL.scala
@@ -1,6 +1,5 @@
 import java.sql.Connection
 import java.sql.DriverManager
-import java.io._
 
 object ScalaJdbcConnectMySQL {
   def main(args: Array[String]): Unit = {
@@ -13,30 +12,16 @@ object ScalaJdbcConnectMySQL {
         connection = DriverManager.getConnection(url, userName, password)
         val statement = connection.createStatement()
         val resultSet = statement.executeQuery("select emp_code, emp_name from employee_master")
-        val metaData = resultSet.getMetaData
-        val columnCount = metaData.getColumnCount
-        val fileObject = new File("/home/subhroneel/Documents/emp_mast.csv")
-        val printWriter = new PrintWriter(fileObject)
-        var colNames = ""
-        for (i <- 1 to columnCount) {
-          colNames = colNames.concat(metaData.getColumnName(i).toUpperCase())
-          if(i<columnCount) {
-            colNames = colNames.concat(",")
-          }
-        }
-        printWriter.write(colNames + "\n")
         while(resultSet.next())
           {
             val empCode = resultSet.getString("emp_code")
             val empName = resultSet.getString("emp_name")
-//            println("Employee Code = " + empCode + ", Employee Name = " + empName)
-            printWriter.write(empCode + "," + empName + "\n")
+            println("Employee Code = " + empCode + ", Employee Name = " + empName)
           }
-        printWriter.close()
         connection.close()
       }
       catch {
         case e: Throwable => e.printStackTrace()
       }
   }
-}
+}       
